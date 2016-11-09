a3ac6d96983a8d6a55dfa14f102ecc44ab1753eb
shawn <shawn@DESKTOP-KV78JM9>
2016-11-08 18:22:32 -0800

    moving project into github repo

---
+++ Code Academy/Code Academy/DAL/SchoolInitializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Code_Academy.Models;
+
+namespace Code_Academy.DAL
+{
+    public class SchoolInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+        protected override void Seed(SchoolContext context)
+        {
+            var students = new List<Student>
+            {
+            new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2015-09-01")},
+            new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2015-09-01")},
+            new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2016-09-01")},
+            new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2016-09-01")},
+            };
+
+            students.ForEach(s => context.Students.Add(s));
+            context.SaveChanges();
+            var courses = new List<Course>
+            {
+            new Course{CourseID=1050,Title="Front End",Credits=3,},
+            new Course{CourseID=4022,Title="Php",Credits=3,},
+            new Course{CourseID=4041,Title="C#",Credits=3,},
+            };
+            courses.ForEach(s => context.Courses.Add(s));
+            context.SaveChanges();
+            var enrollments = new List<Enrollment>
+            {
+            new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
+            new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
+            new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
+            new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
+            new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
+            new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
+            new Enrollment{StudentID=3,CourseID=1050},
+            new Enrollment{StudentID=4,CourseID=1050,},
+           
+            };
+            enrollments.ForEach(s => context.Enrollments.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Code Academy/Code Academy/Models/Course.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Code_Academy.Models
+{
+    public class Course
+    {
+        /// <summary>
+        /// The primary key for courses
+        /// </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        /// <summary>
+        /// The title of the course
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Credits earned for the course
+        /// </summary>
+        public int Credits { get; set; }
+
+        /// <summary>
+        /// List of enrollments in the course
+        /// </summary>
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
+   
